c7ed46e6ee13b94054817b0bd8ce3bcc70777a18
sunil yadav <sunil.yadav@jungleworks.com>
2019-09-17 15:42:32 +0530

    bind api responce data

--- src/main/java/com/sunil/test/activity/ui/HomeActivity.java
+++ src/main/java/com/sunil/test/activity/ui/HomeActivity.java
@@ -4,16 +4,17 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.widget.ImageViewCompat;
-import androidx.core.widget.TextViewCompat;
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.appcompat.widget.AppCompatTextView;
 
+import com.bumptech.glide.Glide;
 import com.sunil.test.R;
 import com.sunil.test.model.MovieDetails;
 
 public class HomeActivity extends AppCompatActivity implements HomeView {
     private HomePresenter homePresenter;
-    private TextViewCompat txtMovieName,txtDate,txtDirector,txtWriter,txtRated,txtDescription;
-    private ImageViewCompat ivMovieBanar;
+    private AppCompatTextView txtMovieName, txtDate, txtDirector, txtWriter, txtRated, txtActors,txtPlot,txtAwards;
+    private AppCompatImageView ivMovieBanar;
 
     @Override
     protected void onCreate(@Nullable final Bundle savedInstanceState) {
@@ -27,6 +28,15 @@ public class HomeActivity extends AppCompatActivity implements HomeView {
     }
 
     private void initComponent() {
+        txtMovieName = findViewById(R.id.txtMovieName);
+        txtDate = findViewById(R.id.txtDate);
+        txtDirector = findViewById(R.id.txtDirector);
+        txtWriter = findViewById(R.id.txtWriter);
+        txtRated = findViewById(R.id.txtRated);
+        txtActors = findViewById(R.id.txtActors);
+        ivMovieBanar = findViewById(R.id.ivMovieBanar);
+        txtPlot = findViewById(R.id.txtPlot);
+        txtAwards = findViewById(R.id.txtAwards);
 
     }
 
@@ -38,6 +48,16 @@ public class HomeActivity extends AppCompatActivity implements HomeView {
 
     @Override
     public void movieDetailsApiResponse(final MovieDetails response) {
+        Glide.with(this).load(response.getPoster()).into(ivMovieBanar);
+        ivMovieBanar.setAdjustViewBounds(true);
+        txtMovieName.setText(response.getTitle());
+        txtDate.setText("Year: "+response.getReleased());
+        txtDirector.setText("Director: "+response.getDirector());
+        txtWriter.setText("Writer: "+response.getWriter());
+        txtRated.setText("Rated: "+response.getRated());
+        txtActors.setText("Actors: "+response.getActors());
+        txtPlot.setText("Plot: "+response.getPlot());
+        txtAwards.setText("Awards: "+response.getAwards());
 
     }
 }
